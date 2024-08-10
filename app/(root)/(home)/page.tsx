158596f37baf015010e55081941af1c8d1f60906
Mohit Mourya <mohitmouryaa143@gmail.com>
2024-08-10 20:52:14 +0530

    authentication

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">Nextjs 13</h1>
+      <UserButton />
     </div>
   );
 }
